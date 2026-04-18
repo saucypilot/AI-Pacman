bb4e0a7a22cec9109d769577b2e164d27b61937a
saucypilot <durianirian@gmail.com>
2026-04-17 20:55:01 -0500

    Q9

--- tracking/inference.py
+++ tracking/inference.py
@@ -579,9 +579,10 @@ class ParticleFilter(InferenceModule):
         self.particles for the list of particles.
         """
         self.particles = []
-        "*** YOUR CODE HERE ***"
-        raiseNotDefined()
-        "*** END YOUR CODE HERE ***"
+
+        numPositions = len(self.legalPositions)
+        for i in range(self.numParticles):
+            self.particles.append(self.legalPositions[i % numPositions])
 
     def getBeliefDistribution(self):
         """
@@ -591,9 +592,13 @@ class ParticleFilter(InferenceModule):
 
         This function should return a normalized distribution.
         """
-        "*** YOUR CODE HERE ***"
-        raiseNotDefined()
-        "*** END YOUR CODE HERE ***"
+        beliefs = DiscreteDistribution()
+
+        for particle in self.particles:
+            beliefs[particle] += 1.0
+
+        beliefs.normalize()
+        return beliefs
     
     ########### ########### ###########
     ########### QUESTION 10 ###########
